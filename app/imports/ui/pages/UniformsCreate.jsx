565f7ba6f06314eaea24d0b2e977624a8dfe9508
YAMADA Masaki <masaki@metabolics.co.jp>
2022-08-25 08:08:04 +0900

    UniformsCreate ページを作る
    中身はまだない

---
+++ app/imports/ui/pages/UniformsCreate.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const UniformsCreate = () => <h1>Uniforms Example</h1>
+
+export default UniformsCreate
--- app/imports/ui/pages/UniformsExample.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const UniformsExample = () => <h1>UniformsExample</h1>
-
-export default UniformsExample
