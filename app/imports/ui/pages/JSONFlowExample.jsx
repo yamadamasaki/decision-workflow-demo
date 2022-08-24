28e8d43be46109a0cbb2775c79ab1d97ece9f537
YAMADA Masaki <masaki@metabolics.co.jp>
2022-08-25 07:19:38 +0900

    JSON Forms 3.0.0-rc.1 + mui 5.10.2 でやってみる
    けど動かない

--- app/imports/ui/pages/JSONFlowExample.jsx
+++ app/imports/ui/pages/JSONFlowExample.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {person} from '@jsonforms/examples'
+import {materialCells, materialRenderers} from '@jsonforms/material-renderers'
+import {JsonForms} from '@jsonforms/react/src/JsonForms'
+
+const schema = person.schema
+const uischema = person.uischema
+const initialData = person.data
 
 const JSONFlowExample = () => {
-  return <h1>JSONFlowExample</h1>
+  const [data, setData] = useState(initialData)
+  return (
+      <>
+        <h1>JSONFlowExample</h1>
+        <JsonForms
+            schema={schema}
+            uischema={uischema}
+            data={data}
+            renderers={materialRenderers}
+            cells={materialCells}
+            onChange={({data, _errors}) => setData(data)}
+        />
+      </>
+  )
 }
 
 export default JSONFlowExample
