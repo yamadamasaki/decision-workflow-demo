7ec56c6553244dfb29548695b14f215eb5f24d30
YAMADA Masaki <masaki@metabolics.co.jp>
2022-08-25 08:28:39 +0900

    とりあえず簡単なフォームを作ってみる
    onSubmit はまだ実装していない

--- app/imports/ui/pages/UniformsCreate.jsx
+++ app/imports/ui/pages/UniformsCreate.jsx
@@ -1,5 +1,39 @@
 import React from 'react'
+import JSONSchemaBridge from 'uniforms-bridge-json-schema'
+import Ajv from 'ajv'
+import {AutoForm} from 'uniforms-bootstrap5'
 
-const UniformsCreate = () => <h1>Uniforms Example</h1>
+const ajv = new Ajv({allErrors: true, useDefaults: true})
+
+const schema = {
+  title: 'Person',
+  type: 'object',
+  properties: {
+    firstName: {type: 'string'},
+    lastName: {type: 'string'},
+    age: {
+      description: 'Age in years',
+      type: 'integer',
+      minimum: 0,
+    },
+  },
+  required: ['firstName', 'lastName'],
+}
+const createValidator = schema => {
+  const validator = ajv.compile(schema)
+
+  return model => {
+    validator(model)
+    return validator.errors?.length ? {details: validator.errors} : null
+  }
+}
+const schemaValidator = createValidator(schema)
+const bridge = new JSONSchemaBridge(schema, schemaValidator)
+
+const UniformsCreate = () => {
+  return (
+      <AutoForm schema={bridge} onSubmit={console.log}/>
+  )
+}
 
 export default UniformsCreate
